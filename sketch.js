a80c3da83ffe5f73d6a6b4282603014ef88cc2b3
ShreyashSwarnkar <82525327+ShreyashSwarnkar@users.noreply.github.com>
2021-06-12 12:01:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -54,7 +54,7 @@ function draw() {
     textSize(windowHeight/10);
     strokeWeight(windowWidth/100-2);
     stroke("black");
-    text("Plz Press Space to Play",windowWidth/4,windowHeight/2);
+    text("Plz Press Space to Play",windowWidth/4,windowHeight/2-100);
     text(":)",windowWidth/2-10,windowHeight/2+100);
     text("You can play by pressing left and right arrow",windowWidth/2,windowHeight/2+200);
     if (keyDown("space") || keyDown("left_arrow") || keyDown("right_arrow")) {
